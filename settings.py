fc03c61f573734bed6952c9b92c147b58e0339af
ssoberin <168803815+ssoberin@users.noreply.github.com>
2025-11-21 15:51:45 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
-api_key = "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"
-token = "8296442320:AAGPUbRNrHxdxfEwivng50FSV2dUKeqTOgc"
-admin_id = 1662659053
+api_key = ""
+token = ""
+# admin_id = 123456789
+
+# добавьте свои ключи в переменные
